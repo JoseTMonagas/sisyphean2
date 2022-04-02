cdc924b17eb9addc36b4e09d7da8efa4b72472f5
Jose Monagas <jose_tadeomong@hotmail.com>
2022-04-02 17:27:56 -0300

    feat: Remove ground from consumer and generator

--- src/power_consumer/PowerConsumer.gd
+++ src/power_consumer/PowerConsumer.gd
@@ -1,15 +1,30 @@
 extends Node2D
 
 export var CONSUMPTION_RATE: int
+export var TTL: float = 60.0
 
 var currentNode: Node2D = null
 var groundNode: Node2D = null
+
+var _state: bool = false
+var _lived: float = 0
 
 onready var currentArea: Area2D = $Current
 onready var groundArea: Area2D = $Ground
 
 func _ready() -> void:
 	assert(CONSUMPTION_RATE > 0)
+	
+	
+func set_power(state: bool) -> bool:
+	if state:
+		_lived = TTL
+	_state = state
+	return state
+
+
+func is_powered() -> bool:
+	return _state
 
 
 func _on_Current_area_entered(area: Area2D) -> void:
@@ -20,11 +35,3 @@ func _on_Current_area_exited(area: Area2D) -> void:
 	if area.get_parent() == currentNode:
 		currentNode = null
 
-
-func _on_Ground_area_entered(area: Area2D) -> void:
-	groundNode = area.get_parent()
-
-
-func _on_Ground_area_exited(area: Area2D) -> void:
-	if area.get_parent() == groundNode:
-		groundNode = null
--- src/power_generator/PowerGenerator.gd
+++ src/power_generator/PowerGenerator.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
 var currentNode: Node2D = null
-var groundNode: Node2D = null
 
 onready var currentArea: Area2D = $Current
-onready var groundArea: Area2D = $Ground
 
 
 func _on_Current_area_entered(area: Area2D) -> void:
@@ -14,12 +12,3 @@ func _on_Current_area_entered(area: Area2D) -> void:
 func _on_Current_area_exited(area: Area2D) -> void:
 	if area.get_parent() == currentNode:
 		currentNode = null
-
-
-func _on_Ground_area_entered(area: Area2D) -> void:
-	groundNode = area.get_parent()
-
-
-func _on_Ground_area_exited(area: Area2D) -> void:
-	if area.get_parent() == groundNode:
-		groundNode = null
