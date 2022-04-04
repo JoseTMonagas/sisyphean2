7b8bd14e8cff4e51850f623e45e21c5769317804
Jose Monagas <jose_tadeomong@hotmail.com>
2022-04-04 15:53:32 -0400

    feat: Add consumer types

--- src/power_consumer/PowerConsumer.gd
+++ src/power_consumer/PowerConsumer.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 export var CONSUMPTION_RATE: int
-export var TTL: float = 60.0
+export var ACTIVE_ANIMATION: String
+export var INACTIVE_ANIMATION: String
+export var TTL: float = 15.0
+export var SCORE_MULTIPLIER: int = 2
 
 var currentNode: Node2D = null
 var groundNode: Node2D = null
@@ -10,15 +13,27 @@ var _state: bool = false
 var _lived: float = 0
 
 onready var currentArea: Area2D = $Current
-onready var groundArea: Area2D = $Ground
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	assert(CONSUMPTION_RATE > 0)
+	assert(ACTIVE_ANIMATION)
+	assert(INACTIVE_ANIMATION)
 	
+	
+func _physics_process(delta: float) -> void:
+	if not is_powered():
+		_lived -= delta
+		if _lived <= 0:
+			set_power(false)
+		
 	
 func set_power(state: bool) -> bool:
 	if state:
 		_lived = TTL
+		animationPlayer.play(ACTIVE_ANIMATION)
+	else:
+		animationPlayer.play(INACTIVE_ANIMATION)
 	_state = state
 	return state
 
